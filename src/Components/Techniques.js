755dac40da2c9f23e0055dc9c6d1f033b9a7c9ae
Cari <t.carina@rocketmail.com>
2021-08-22 01:34:17 -0400

    make style changes

--- src/Components/Techniques.js
+++ src/Components/Techniques.js
@@ -21,7 +21,7 @@ export default function Techniques() {
                             <div className="modal-div">
                                 <h3 className="carousel-h3">Walk or do light exercise</h3>
                                 <p className="carousel-p">Walking can remove a person from a stressful environment, and the rhythm of walking may also help them regulate their breathing. Moving around releases hormones called endorphins that relax the body and improve mood. Taking up regular exercise can help reduce anxiety over time, which may lead to a reduction in the number or severity of panic attacks.</p>
-                                <a href="https://www.medicalnewstoday.com/articles/153390">Learn more here</a>
+                                <a href="https://www.medicalnewstoday.com/articles/153390" className='tLink'>Learn more here</a>
 
                             </div>
                         </Carousel.Caption>
@@ -41,7 +41,7 @@ export default function Techniques() {
                                     Touch 3 objects. Consider their texture, temperature, and what their uses are.
                                     Identify 2 different smells. This could be the smell of your coffee, your soap, or the laundry detergent on your clothes.
                                     Name 1 thing you can taste. Notice whatever taste is in your mouth, or try tasting a piece of candy.</p>
-                                <a href="https://images.squarespace-cdn.com/content/v1/5536830ce4b04567e58178fa/1531767643952-UCZO3TBAW6WIIJLFJHUK/Barefoot-Grass-Walking-Health.jpg">Learn more here</a>
+                                <a href="https://images.squarespace-cdn.com/content/v1/5536830ce4b04567e58178fa/1531767643952-UCZO3TBAW6WIIJLFJHUK/Barefoot-Grass-Walking-Health.jpg" className='tLink'>Learn more here</a>
                             </div></Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item interval={10000}>
@@ -59,7 +59,7 @@ export default function Techniques() {
                                       obsessing about something. You can also ask yourself some questions. Is the thing you are afraid of a true and present danger?
                                        Realize that you are experiencing fear, but that you are not in danger. Taking the danger out of the situation will help you
                                         to relax a bit</p>
-                                <a href="https://www.wikihow.com/Calm-Yourself-During-an-Anxiety-Attack">Learn more here</a>
+                                <a href="https://www.wikihow.com/Calm-Yourself-During-an-Anxiety-Attack" className='tLink'>Learn more here</a>
                             </div></Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
